d098d4865df887d3730eb1db9c1ea6a2c4d3ccc1
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-02-18 22:00:09 +0600

    login functionality added using javascript

--- js/login.js
+++ js/login.js
@@ -1,1 +1,17 @@
-console.log('login connected')
+document.getElementById("login-btn").addEventListener("click", function () {
+  // input number
+  const inputNumber = document.getElementById("input-number");
+  const inputNumberValue = inputNumber.value;
+
+  // input pin
+  const inputPin = document.getElementById("input-pin");
+  const inputPinValue = inputPin.value;
+
+  if (inputNumberValue == "01234567890" && inputPinValue == "1234") {
+    alert("Login Successful!");
+    window.location.assign("/home.html");
+  } else {
+    alert("Login Failed. Invalid Mobile Number or Pin!");
+    return;
+  }
+});
